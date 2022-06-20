b2def4923129188c93e51567442835cb3b6b3d3c
dileep kumar <dileepgudimetla9490@gmail.com>
2022-06-20 05:39:52 -0700

    Update config.go

--- config/config.go
+++ config/config.go
@@ -2,14 +2,8 @@ package config
 
 import (
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func Config(key string) string {
-
-	if err := godotenv.Load(".env"); err != nil {
-		panic("error in loading .env file")
-	}
 	return os.Getenv(key)
 }
